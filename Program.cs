87044bcdbb1e6ebc7313e034fdc6a794fa12c32e
Humberto Rodrigues <humberto.fcamara@superdigital.com.br>
2020-10-29 01:20:25 -0300

    starting

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_DotNet_Polymorphism
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class
+}
